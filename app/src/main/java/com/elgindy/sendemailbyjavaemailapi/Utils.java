05b247454ba22722887210481a01431fbf875e46
Md. Talat Mahmud <md.talatcse@gmail.com>
2021-03-10 13:42:50 +0600

    first master commit

---
+++ app/src/main/java/com/elgindy/sendemailbyjavaemailapi/Utils.java
@@ -0,0 +1,8 @@
+package com.elgindy.sendemailbyjavaemailapi;
+
+public class Utils {
+    // HERE PUT EMAIL AND PASSWORD , LIKE example@gmail.com
+
+    public static final  String EMAIL = "freshexport.order@gmail.com";
+    public static final String PASSWORD = "freshExport@123";
+}
